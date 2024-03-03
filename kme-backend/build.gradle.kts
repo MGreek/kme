51dba8a62a152a38dc5ac2dd649e84d402845436
MGreek <marcgrec2003@gmail.com>
2024-03-03 14:25:46 +0200

    chore: configure spring boot via application.yml
    Created two spring profiles. One is for testing and the other for
    development. They require env vars to work.

--- kme-backend/build.gradle.kts
+++ kme-backend/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
