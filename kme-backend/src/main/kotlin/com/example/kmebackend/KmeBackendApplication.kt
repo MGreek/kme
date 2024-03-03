99afcb58a8b942902b53db933b51d2cb865df351
MGreek <marcgrec2003@gmail.com>
2024-03-03 12:46:41 +0200

    regenerate backend
    I used spring initializr to make this spring boot project use kotlin and
    gradle with kotlin instead of java and groovy.

---
+++ kme-backend/src/main/kotlin/com/example/kmebackend/KmeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kmebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KmeBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<KmeBackendApplication>(*args)
+}
