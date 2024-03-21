cebd4c8101c5874ca2c40264958e434be5e43455
MGreek <marcgrec2003@gmail.com>
2024-03-21 21:06:26 +0200

    feat: add vexflow example

--- kme-frontend/src/app/app.component.ts
+++ kme-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Vex } from 'vexflow';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kme-frontend';
+  
+  ngOnInit() {
+    const { Factory, EasyScore, System } = Vex.Flow;
+
+    const vf = new Factory({
+      renderer: { elementId: 'output', width: 500, height: 200 },
+    });
+
+    const score = vf.EasyScore();
+    const system = vf.System();
+
+    system
+      .addStave({
+        voices: [
+          score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
+          score.voice(score.notes('C#4/h, C#4', { stem: 'down' })),
+        ],
+      })
+      .addClef('treble')
+      .addTimeSignature('4/4');
+
+    vf.draw();
+  }
 }
