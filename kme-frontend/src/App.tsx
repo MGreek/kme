a86ae25c41057176b414fad4ca42922163d1d39b
MGreek <marcgrec2003@gmail.com>
2024-04-05 20:32:17 +0300

    feat: render sample music

--- kme-frontend/src/App.tsx
+++ kme-frontend/src/App.tsx
@@ -1,12 +1,35 @@
 import { useEffect } from "react";
-import { drawSample } from "vexflow-repl"
+import {
+	type FullRenderOptions,
+	fullRender,
+	type StaffSystem,
+} from "vexflow-repl";
+import request from "./api/request";
 
 export default function App() {
-  useEffect(() => {
-    const div = document.getElementById("output")
-    if (div instanceof HTMLDivElement) {
-      drawSample(div)
-    }
-  }, [])
-  return <div id="output"></div>
+	useEffect(() => {
+		request<StaffSystem>("GET", "/api/staff-system/sample", {}).then(
+			(response) => {
+				console.log(JSON.stringify(response.data));
+				const div = document.getElementById("output");
+				if (div instanceof HTMLDivElement) {
+					const options: FullRenderOptions = {
+						totalWidth: 400,
+						totalHeight: 800,
+						defaultX: 15,
+						defaultY: 0,
+						defaultStaveWidth: 350,
+						defaultSystemGap: 20,
+					};
+					fullRender(div.id, response.data, options);
+				}
+			},
+		);
+
+		// const div = document.getElementById("output")
+		// if (div instanceof HTMLDivElement) {
+		//   drawSample(div)
+		// }
+	}, []);
+	return <div id="output" />;
 }
