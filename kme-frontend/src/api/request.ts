e91c514a40e7296c6dbe900adbbab2404dddd6c6
MGreek <marcgrec2003@gmail.com>
2024-04-05 20:29:20 +0300

    feat: make http-request utility function

---
+++ kme-frontend/src/api/request.ts
@@ -0,0 +1,17 @@
+import axios, { Method, AxiosResponse } from "axios";
+
+const api = axios.create({
+  baseURL: process.env.BACKEND_HOST,
+});
+
+export default function request<T>(
+  method: Method,
+  url: string,
+  params: unknown
+): Promise<AxiosResponse<T>> {
+  return api.request<T>({
+    method,
+    url,
+    params,
+  });
+};
