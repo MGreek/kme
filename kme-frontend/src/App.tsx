ce175e536b260e23c077637cba9ace60c1377e91
MGreek <marcgrec2003@gmail.com>
2024-04-06 22:51:16 +0300

    feat: display sample music

--- kme-frontend/src/App.tsx
+++ kme-frontend/src/App.tsx
@@ -1,35 +1,67 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
-	type FullRenderOptions,
-	fullRender,
-	type StaffSystem,
+  type FullRenderOptions,
+  fullRender,
+  type StaffSystem,
 } from "vexflow-repl";
 import request from "./api/request";
+import { Factory } from "vexflow";
+
+interface State {
+  system: StaffSystem | null;
+  flip: boolean;
+}
 
 export default function App() {
-	useEffect(() => {
-		request<StaffSystem>("GET", "/api/staff-system/sample", {}).then(
-			(response) => {
-				console.log(JSON.stringify(response.data));
-				const div = document.getElementById("output");
-				if (div instanceof HTMLDivElement) {
-					const options: FullRenderOptions = {
-						totalWidth: 400,
-						totalHeight: 800,
-						defaultX: 15,
-						defaultY: 0,
-						defaultStaveWidth: 350,
-						defaultSystemGap: 20,
-					};
-					fullRender(div.id, response.data, options);
-				}
-			},
-		);
+  const factoryRef = useRef<Factory | null>(null);
+  const [state, setState] = useState<State>({ system: null, flip: false });
 
-		// const div = document.getElementById("output")
-		// if (div instanceof HTMLDivElement) {
-		//   drawSample(div)
-		// }
-	}, []);
-	return <div id="output" />;
+  const onClick = () => {
+    if (state.system != null) {
+      state.system.staves.pop();
+    }
+    setState({ system: state.system, flip: !state.flip });
+  };
+
+  useEffect(() => {
+    request<StaffSystem>("GET", "/api/staff-system/sample", {}).then(
+      (response) => {
+        setState({ system: response.data, flip: true });
+      },
+    );
+
+    // const div = document.getElementById("output")
+    // if (div instanceof HTMLDivElement) {
+    //   drawSample(div)
+    // }
+  }, []);
+  if (state.system != null) {
+    const div = document.getElementById("output");
+    if (div instanceof HTMLDivElement) {
+      const options: FullRenderOptions = {
+        totalWidth: 1200,
+        totalHeight: 600,
+        x: 15,
+        y: 0,
+        defaultStaveWidth: 350,
+        defaultSystemGap: 12,
+      };
+      if (factoryRef.current == null) {
+        factoryRef.current = Factory.newFromElementId(
+          div.id,
+          options.totalWidth,
+          options.totalHeight,
+        );
+      }
+      fullRender(factoryRef.current, state.system, options);
+    }
+  }
+  return (
+    <>
+      <button type="button" onClick={onClick}>
+        Pop a stave
+      </button>
+      <div id="output" />
+    </>
+  );
 }
