3383818a7925fbcef6c51039750df399dae33d55
MGreek <marcgrec2003@gmail.com>
2024-04-27 17:51:54 +0300

    style: use space instead of tab

--- kme-frontend/src/api/request.ts
+++ kme-frontend/src/api/request.ts
@@ -1,17 +1,17 @@
 import axios, { type Method, type AxiosResponse } from "axios";
 
 const api = axios.create({
-	baseURL: process.env.BACKEND_HOST,
+  baseURL: process.env.BACKEND_HOST,
 });
 
 export default function request<T>(
-	method: Method,
-	url: string,
-	params: unknown,
+  method: Method,
+  url: string,
+  params: unknown,
 ): Promise<AxiosResponse<T>> {
-	return api.request<T>({
-		method,
-		url,
-		params,
-	});
+  return api.request<T>({
+    method,
+    url,
+    params,
+  });
 }
