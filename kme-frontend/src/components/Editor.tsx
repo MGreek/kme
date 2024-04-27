f097117c08af705c9a6380492f469c8089995b1e
MGreek <marcgrec2003@gmail.com>
2024-04-27 17:56:32 +0300

    feat: use new vexflow-repl interface

--- kme-frontend/src/components/Editor.tsx
+++ kme-frontend/src/components/Editor.tsx
@@ -1,49 +1,46 @@
 import { useEffect, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { Factory } from "vexflow";
+import { SVGContext, type RenderContext } from "vexflow";
 import {
-	type FullRenderOptions,
-	type StaffSystem,
-	fullRender,
+  type RenderOptions,
+  renderStaffSystemAtIndex,
+  type StaffSystem,
 } from "vexflow-repl";
 
-interface FactoryRef {
-	factory: Factory | null;
-}
-
 export default function Editor({ staffSystem }: { staffSystem: StaffSystem }) {
-	const factoryRef = useRef<FactoryRef>({ factory: null });
-	const divRef = useRef<HTMLDivElement>(null);
+  const renderContextRef = useRef<RenderContext>();
+  const divRef = useRef<HTMLDivElement>(null);
 
-	function renderStaffSystem() {
-		const div = divRef.current;
-		if (div == null) {
-			return;
-		}
-		div.id = uuidv4();
+  function renderStaffSystem() {
+    const div = divRef.current;
+    if (div == null) {
+      return;
+    }
+    div.id = uuidv4();
 
-		const options: FullRenderOptions = {
-			totalWidth: div.clientWidth,
-			totalHeight: div.clientHeight,
-			x: 15,
-			y: 0,
-			defaultStaveWidth: 350,
-			defaultSystemGap: 12,
-		};
+    const options: RenderOptions = {
+      totalWidth: div.clientWidth,
+      totalHeight: div.clientHeight,
+      x: 50,
+      y: 0,
+      defaultStaveWidth: 350,
+      defaultSystemGap: 12,
+      clear: true,
+      drawConnector: true,
+    };
 
-		if (factoryRef.current.factory == null) {
-			factoryRef.current.factory = Factory.newFromElementId(
-				div.id,
-				div.clientWidth,
-				div.clientHeight,
-			);
-		}
-		fullRender(factoryRef.current.factory, staffSystem, options);
-	}
+    if (renderContextRef.current == null) {
+      renderContextRef.current = new SVGContext(div).resize(
+        div.clientWidth,
+        div.clientHeight,
+      );
+    }
+    renderStaffSystemAtIndex(renderContextRef.current, staffSystem, 0, options);
+  }
 
-	useEffect(() => {
-		renderStaffSystem();
-	}, []);
+  useEffect(() => {
+    renderStaffSystem();
+  }, [renderStaffSystem]);
 
-	return <div className="sheetmusic-page mx-auto bg-blue-50" ref={divRef} />;
+  return <div className="sheetmusic-page mx-auto bg-blue-50" ref={divRef} />;
 }
