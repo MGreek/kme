5572321075fedfe53dd5a65958c902c54f24e9e7
MGreek <marcgrec2003@gmail.com>
2024-05-02 18:00:52 +0300

    feat: create onWrapEnter callback for Row

--- kme-frontend/src/components/Page.tsx
+++ kme-frontend/src/components/Page.tsx
@@ -1,15 +1,38 @@
 import type { StaffSystem } from "vexflow-repl";
 import Row from "./Row";
+import { useCallback, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 interface PageProps {
   staffSystem: StaffSystem;
 }
 
 export default function Page({ staffSystem }: PageProps) {
+  const [rows, setRows] = useState<JSX.Element[]>([]);
+
+  const onRenderRef = useCallback(
+    (div: HTMLDivElement | null) => {
+      if (div != null) {
+        const newRow = (
+          <Row
+            key={uuidv4()}
+            staffSystem={staffSystem}
+            bounds={div.getBoundingClientRect()}
+          />
+        );
+        setRows([newRow]);
+      }
+    },
+    [staffSystem],
+  );
+
   return (
     <>
-      <div className="sheetmusic-page flex flex-col items-start justify-start bg-blue-50">
-        <Row staffSystem={staffSystem} />
+      <div
+        ref={onRenderRef}
+        className="sheetmusic-page flex flex-col items-start justify-start bg-blue-50"
+      >
+        {rows}
       </div>
     </>
   );
