c595e1b0aca40be69ab9c4bb72e0b87b54cd54d7
MGreek <marcgrec2003@gmail.com>
2024-05-03 16:10:30 +0300

    feat: deharcode appending of chunks

--- kme-frontend/src/components/Row.tsx
+++ kme-frontend/src/components/Row.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import type { StaffSystem } from "../model/staff-system";
 import Chunk from "./Chunk";
@@ -10,30 +10,34 @@ interface RowProps {
 
 export default function Row({ staffSystem, bounds }: RowProps) {
   const [chunks, setChunks] = useState<JSX.Element[]>([]);
+  const stopAppendingRef = useRef<boolean>(false);
 
-  const onWrapEnter = (chunkIndex: number) => {
-    console.log(chunkIndex);
+  const onOutOfBounds = (
+    chunkIndex: number,
+    widthExceeded: boolean,
+    heightExceeded: boolean,
+  ) => {
+    stopAppendingRef.current = true;
+    setChunks([...chunks.slice(0, chunkIndex)]);
   };
 
   const onRenderRef = useCallback(
     (div: HTMLDivElement | null) => {
-      if (div == null) {
+      if (stopAppendingRef.current || div == null) {
         return;
       }
-      if (chunks.length < 3) {
-        const newChunk = (
-          <Chunk
-            key={uuidv4()}
-            staffSystem={staffSystem}
-            chunkIndex={chunks.length}
-            bounds={bounds}
-            onWrapEnter={onWrapEnter}
-          />
-        );
-        setChunks([...chunks, newChunk]);
-      }
+      const newChunk = (
+        <Chunk
+          key={uuidv4()}
+          staffSystem={staffSystem}
+          chunkIndex={chunks.length}
+          bounds={bounds}
+          onOutOfBounds={onOutOfBounds}
+        />
+      );
+      setChunks([...chunks, newChunk]);
     },
-    [chunks, chunks.length, onWrapEnter, staffSystem, bounds],
+    [chunks, chunks.length, onOutOfBounds, staffSystem, bounds],
   );
 
   return (
