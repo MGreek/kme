ddf450a47ad44e882ea246e596fb392548533941
MGreek <marcgrec2003@gmail.com>
2024-05-09 18:50:13 +0300

    feat: add element for measuring Chunk elements
    This element takes a `StaffSystem` and renders `Chunk` elements derived
    from this `StaffSystem` in an invisible `div`.
    
    It waits for the `Chunk` elements to *callback* so it can gather info
    about size and stave-alignment.

---
+++ kme-frontend/src/components/ChunksMeasurement.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { StaffSystem } from "../model/staff-system";
+import { getChunksFromStaffSystem } from "../util/misc";
+import { requireNotNull } from "../util/require-not-null";
+
+export interface ChunksMeasurementProps {
+  staffSystem: StaffSystem;
+  onMeasurements: (measurements: Measurements) => void;
+}
+
+export interface Measurements {
+  widths: number[];
+  heights: number[];
+  chunksStavesYs: Array<Array<number>>;
+}
+
+export default function ChunksMeasurement({
+  staffSystem,
+  onMeasurements,
+}: ChunksMeasurementProps) {
+  const measurementsRef = useRef<Measurements>({
+    widths: [],
+    heights: [],
+    chunksStavesYs: [],
+  });
+  const chunksRef = useRef<JSX.Element[] | null>(null);
+  const [chunksToRender, setChunksToRender] = useState<JSX.Element[] | null>(
+    null,
+  );
+
+  const onChunkRenderRef = useCallback(
+    (
+      chunkIndex: number,
+      width: number,
+      height: number,
+      chunkStavesYs: number[],
+    ) => {
+      measurementsRef.current.widths.push(width);
+      measurementsRef.current.heights.push(height);
+      measurementsRef.current.chunksStavesYs.push(chunkStavesYs);
+
+      const chunks = requireNotNull(chunksRef.current);
+      if (chunkIndex === chunks.length - 1) {
+        onMeasurements(measurementsRef.current);
+      }
+    },
+    [onMeasurements],
+  );
+
+  useEffect(() => {
+    measurementsRef.current = { widths: [], heights: [], chunksStavesYs: [] };
+    chunksRef.current = getChunksFromStaffSystem(staffSystem, onChunkRenderRef);
+    if (chunksRef.current.length === 0) {
+      onMeasurements(measurementsRef.current);
+    }
+    setChunksToRender(chunksRef.current);
+  }, [staffSystem, onChunkRenderRef, onMeasurements]);
+
+  return <div className="hidden">{chunksToRender}</div>;
+}
