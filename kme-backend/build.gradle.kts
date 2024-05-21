41bb6725a40c3bf4ee086e20978238d18dbc0047
MGreek <marcgrec2003@gmail.com>
2024-05-21 17:55:35 +0300

    build: add MapStruct

--- kme-backend/build.gradle.kts
+++ kme-backend/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.jmailen.kotlinter") version "4.2.0"
+    kotlin("kapt") version "2.0.0"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
@@ -21,6 +22,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.mapstruct:mapstruct:1.6.0.Beta2")
+    kapt("org.mapstruct:mapstruct-processor:1.6.0.Beta2")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -28,6 +31,12 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+kapt {
+    arguments {
+        arg("mapstruct.defaultComponentModel", "spring")
+    }
 }
 
 tasks.withType<KotlinCompile> {
