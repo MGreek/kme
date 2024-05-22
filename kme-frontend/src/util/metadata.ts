ab66ca6cfd52cbe5459234b157f1c8e6493fbc3b
MGreek <marcgrec2003@gmail.com>
2024-05-22 21:34:21 +0300

    feat: create metadata parsers

---
+++ kme-frontend/src/util/metadata.ts
@@ -0,0 +1,63 @@
+import type { Measure } from "../model/measure";
+import { ConnectorType, type StaffSystem } from "../model/staff-system";
+
+export function parseStaffSystemMetadata(staffSystem: StaffSystem): {
+  connectorType: ConnectorType;
+} {
+  let object = null;
+  try {
+    object = JSON.parse(staffSystem.metadataJson);
+  } catch { }
+
+  let connectorType = ConnectorType.None;
+  if (
+    object != null &&
+    "connectorType" in object &&
+    typeof object.connectorType === "string" &&
+    Object.values(ConnectorType).includes(connectorType)
+  ) {
+    connectorType = object.connectorType;
+  }
+
+  return { connectorType };
+}
+
+export function parseMeasureMetadata(measure: Measure): {
+  drawClef: boolean;
+  drawKeySignature: boolean;
+  drawTimeSignature: boolean;
+} {
+  let object = null;
+  try {
+    object = JSON.parse(measure.metadataJson);
+  } catch (error) { }
+
+  let drawClef = false;
+  if (
+    object != null &&
+    "drawClef" in object &&
+    typeof object.drawClef === "boolean"
+  ) {
+    drawClef = object.drawClef;
+  }
+
+  let drawKeySignature = false;
+  if (
+    object != null &&
+    "drawKeySignature" in object &&
+    typeof object.drawClef === "boolean"
+  ) {
+    drawKeySignature = object.drawKeySignature;
+  }
+
+  let drawTimeSignature = false;
+  if (
+    object != null &&
+    "drawTimeSignature" in object &&
+    typeof object.drawClef === "boolean"
+  ) {
+    drawTimeSignature = object.drawTimeSignature;
+  }
+
+  return { drawClef, drawKeySignature, drawTimeSignature };
+}
