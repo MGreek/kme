7a2b203c36e66086ff8b7a6594de9c9a7d74de53
MGreek <marcgrec2003@gmail.com>
2024-05-22 19:47:11 +0300

    feat(GroupingEntryRepository): add child-getters by ID

--- kme-backend/src/main/kotlin/com/example/kmebackend/repository/GroupingEntryRepository.kt
+++ kme-backend/src/main/kotlin/com/example/kmebackend/repository/GroupingEntryRepository.kt
@@ -1,9 +1,18 @@
 package com.example.kmebackend.repository
 
+import com.example.kmebackend.model.Chord
 import com.example.kmebackend.model.GroupingEntry
 import com.example.kmebackend.model.GroupingEntryId
+import com.example.kmebackend.model.Rest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GroupingEntryRepository : JpaRepository<GroupingEntry, GroupingEntryId>
+interface GroupingEntryRepository : JpaRepository<GroupingEntry, GroupingEntryId> {
+    @Query("SELECT r FROM Rest r WHERE r.restId.groupingEntryId = ?1")
+    fun getRest(groupingEntryId: GroupingEntryId): Rest?
+
+    @Query("SELECT c FROM Chord c WHERE c.chordId.groupingEntryId = ?1")
+    fun getChord(groupingEntryId: GroupingEntryId): Chord?
+}
