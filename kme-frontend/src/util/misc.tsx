2a40652ba8fca8671761ce01a43d11171ccda175
MGreek <marcgrec2003@gmail.com>
2024-05-27 16:25:13 +0300

    fix(misc): remove unused function

--- kme-frontend/src/util/misc.tsx
+++ kme-frontend/src/util/misc.tsx
@@ -1,17 +1,4 @@
 import type { StaffSystem } from "../model/staff-system";
 import { requireNotNull } from "./require-not-null";
 
-export function getStaffSystemAtIndex(
-  staffSystem: StaffSystem,
-  index: number,
-): StaffSystem {
-  const staffSystemClone = structuredClone(staffSystem);
-  staffSystemClone.staves = staffSystem.staves.map((staff) => {
-    const staffClone = structuredClone(staff);
-    staffClone.measures = [
-      structuredClone(requireNotNull(staff.measures.at(index))),
-    ];
-    return staffClone;
-  });
-  return staffSystemClone;
 }
