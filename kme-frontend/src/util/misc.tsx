cf547e64cb2d2fbeb8e3c7a10610430955eb2c6a
MGreek <marcgrec2003@gmail.com>
2024-05-27 16:25:39 +0300

    feat(misc): add getStaffSystemMeasureCount

--- kme-frontend/src/util/misc.tsx
+++ kme-frontend/src/util/misc.tsx
@@ -1,4 +1,18 @@
 import type { StaffSystem } from "../model/staff-system";
 import { requireNotNull } from "./require-not-null";
 
+export function getStaffSystemMeasureCount(staffSystem: StaffSystem) {
+  if (staffSystem.staves.length === 0) {
+    return 0;
+  }
+
+  const measureCount = requireNotNull(staffSystem.staves[0]).measures.length;
+
+  if (
+    staffSystem.staves.some((staff) => staff.measures.length !== measureCount)
+  ) {
+    throw new Error("All staves must have the same number of measures");
+  }
+
+  return measureCount;
 }
