2df73ea950a37e626003aa226873fd91e3e0ccc9
MGreek <marcgrec2003@gmail.com>
2024-05-29 22:35:23 +0300

    feat(App): set pagePadding values

--- kme-frontend/src/App.tsx
+++ kme-frontend/src/App.tsx
@@ -23,10 +23,10 @@ export default function App() {
         staffSystem={dataReceived}
         pageGap={20}
         pagePadding={{
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
+          left: 20,
+          right: 20,
+          top: 20,
+          bottom: 20,
         }}
       />
     </div>
