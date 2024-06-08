b13ab912cebdfaf29eb3252df95564803f3f86ef
MGreek <marcgrec2003@gmail.com>
2024-06-08 11:04:02 +0300

    feat(request): add get/set API for staff system

--- kme-frontend/src/api/request.ts
+++ kme-frontend/src/api/request.ts
@@ -1,4 +1,5 @@
 import axios, { type Method, type AxiosResponse } from "axios";
+import type { StaffSystem, StaffSystemId } from "../model/staff-system";
 
 const api = axios.create({
   baseURL: process.env.BACKEND_HOST,
@@ -14,4 +15,35 @@ export default function request<T>(
     url,
     params,
   });
+}
+
+export function onGetStaffSystemById(
+  staffSystemId: StaffSystemId | null,
+  callback: (staffSystem: StaffSystem | null) => void,
+) {
+  let url = "/api/staff-system/";
+  if (staffSystemId == null) {
+    url += "sample";
+  } else {
+    url += staffSystemId.staffSystemId;
+  }
+  request<StaffSystem>("GET", url, {}).then(
+    (response) => {
+      callback(response.data);
+    },
+    () => {
+      callback(null);
+    },
+  );
+}
+
+export function onSetStaffSystemById(
+  staffSystem: StaffSystem,
+  callback: (success: boolean) => void,
+) {
+  const url = "/api/staff-system/set";
+  api
+    .post(url, staffSystem)
+    .then(() => callback(true))
+    .catch(() => callback(false));
 }
