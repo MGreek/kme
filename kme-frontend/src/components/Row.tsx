de34ec1cbfc111a6b1d942eae81b5180a0dccc7c
MGreek <marcgrec2003@gmail.com>
2024-06-11 16:32:44 +0300

    fix(Row): add staffSystemId to getRowJson

--- kme-frontend/src/components/Row.tsx
+++ kme-frontend/src/components/Row.tsx
@@ -256,6 +256,7 @@ export function getRowJson(
     totalWidth,
     startMeasureIndex,
     stopMeasureIndex,
+    staffSystemId: staffSystem.staffSystemId,
     staffSystemMetadata: parseStaffSystemMetadata(staffSystem),
     staves: staffSystem.staves.map((staff) => {
       const newStaff: Staff = {
