0cb25b90a6bfa0804dc21dd8fe2bb0ac32a0e557
MGreek <marcgrec2003@gmail.com>
2024-06-23 14:09:49 +0300

    feat(App): allow switching between Editor and Explorer

--- kme-frontend/src/App.tsx
+++ kme-frontend/src/App.tsx
@@ -1,26 +1,64 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import Editor from "./components/Editor";
 import Explorer from "./components/Explorer";
 import type { StaffSystem } from "./model/staff-system";
-import Editor from "./components/Editor";
+import { requireNotNull } from "./util/require-not-null";
+import { onSetStaffSystemById } from "./api/request";
+
+enum ComponentType {
+  Explorer = "Explorer",
+  Editor = "Editor",
+}
 
 export default function App() {
-  const onStaffSystemSelected = useCallback((staffSystem: StaffSystem) => {
-    setComponent(
-      <Editor
-        pagePadding={{
-          left: 20,
-          right: 20,
-          top: 20,
-          bottom: 20,
-        }}
-        initialStaffSystem={staffSystem}
-      />,
-    );
-  }, []);
-
-  const [component, setComponent] = useState<JSX.Element>(
-    <Explorer onOpen={onStaffSystemSelected} />,
+  const staffSystemRef = useRef<StaffSystem | null>(null);
+  const [componentType, setComponentType] = useState<ComponentType>(
+    ComponentType.Explorer,
   );
+
+  let component: JSX.Element | null = null;
+  switch (componentType) {
+    case ComponentType.Explorer:
+      component = (
+        <Explorer
+          onOpen={(staffSystem) => {
+            staffSystemRef.current = staffSystem;
+            setComponentType(ComponentType.Editor);
+          }}
+        />
+      );
+      break;
+    case ComponentType.Editor:
+      component = (
+        <Editor
+          pagePadding={{
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+          }}
+          initialStaffSystem={requireNotNull(
+            staffSystemRef.current,
+            "Expected staffSystemRef to be initialized",
+          )}
+          onExplore={() => {
+            setComponentType(ComponentType.Explorer);
+          }}
+          onWrite={(staffSystem: StaffSystem) => {
+            onSetStaffSystemById(staffSystem, (success) => {
+              if (!success) {
+                alert(
+                  `Failed to persist staff system with id: ${staffSystem.staffSystemId.staffSystemId}`,
+                );
+              }
+            });
+          }}
+        />
+      );
+      break;
+    default:
+      throw new Error("Unknown component type");
+  }
 
   return component;
 }
